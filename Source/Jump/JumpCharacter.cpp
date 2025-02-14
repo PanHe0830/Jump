68ec5977d67ca42f04d90a58a70dbd79dd8bb469
panhe125801@gmail.com <panhe125801@gmail.com>
2025-02-14 22:52:40 +0800

    创建生成/删除运动的形状的类，尝试FActorIterator,FActorRange,TActorIterator

--- Source/Jump/JumpCharacter.cpp
+++ Source/Jump/JumpCharacter.cpp
@@ -42,7 +42,7 @@ AJumpCharacter::AJumpCharacter()
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
+	CameraBoom->TargetArmLength = 800.0f; // The camera follows at this distance behind the character	
 	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
 	// Create a follow camera
